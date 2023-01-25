603e538443960831b31b9890cefdf53ecdc4ef6e
Shohkrukh Turaev <turaevshokhrukh7@gmail.com>
2023-01-25 23:05:52 +0500

    login step done!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,60 @@
-import "./App.css";
+import { useState } from "react";
+import LoginStep from "./components/login-step";
 
 function App() {
+  const [isFormValid, setIsFormValid] = useState(false);
   return (
-    <div>
-      <div></div>
+    <div className="app">
+      <div className="wizard-container">
+        <div className="title-bar">
+          <span className="witch-hat-container">
+            <img alt="witch hat" src="witch-hat.png" width={32} height={32} />{" "}
+            My Wizard
+          </span>
+          <span>Step 2 of 4</span>
+        </div>
+        <LoginStep
+          onValidityChange={(isValid: boolean) => setIsFormValid(isValid)}
+        />
+        <div className="nav-steps-bar">
+          <button type="button" className="button-text">
+            Cancel
+          </button>
+          <div style={{ display: "flex", gap: "0.3rem" }}>
+            <button type="button" className="button-text" disabled>
+              <img
+                alt="Chevron left"
+                src="chevron-left.png"
+                height={16}
+                width={16}
+                style={{ textAlign: "center" }}
+              />{" "}
+              Previous step
+            </button>
+            <button
+              form="login-step"
+              type="button"
+              className="button-primary"
+              disabled={!isFormValid}
+              onClick={(e) => {
+                let formData: FormData;
+                if (e.currentTarget.form !== null) {
+                  formData = new FormData(e.currentTarget.form);
+                  console.log(formData.get("username"));
+                }
+              }}
+            >
+              Next step{" "}
+              <img
+                alt="Chevron right"
+                src="chevron-right.png"
+                height={16}
+                width={16}
+              />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/login-step.tsx
@@ -0,0 +1,173 @@
+import { useState } from "react";
+
+export default function LoginStep({
+  onValidityChange: setFormValidity,
+}: {
+  onValidityChange: (inValid: boolean) => void;
+}) {
+  const [username, setUsername] = useState({ valid: false, untouched: true });
+  const [password, setPassword] = useState({
+    valid: false,
+    value: "",
+    untouched: true,
+  });
+  const [confirmPassword, setConfirmPassword] = useState({
+    valid: false,
+    value: "",
+    untouched: true,
+  });
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername({
+      ...username,
+      valid: e.target.validity.valid,
+    });
+
+    setFormValidity(
+      e.target.validity.valid && password.valid && confirmPassword.valid
+    );
+  };
+
+  const handlePwdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isConfirmPwdValid =
+      confirmPassword.value !== "" && e.target.value === confirmPassword.value;
+
+    setPassword({
+      ...password,
+      valid: e.target.validity.valid,
+      value: e.target.value,
+    });
+    setConfirmPassword({
+      ...confirmPassword,
+      valid: isConfirmPwdValid,
+    });
+
+    setFormValidity(
+      username.valid && e.target.validity.valid && isConfirmPwdValid
+    );
+  };
+
+  const handleConfirmPwdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword({
+      ...confirmPassword,
+      valid: e.target.validity.valid,
+      value: e.target.value,
+    });
+
+    setFormValidity(
+      username.valid && password.valid && e.target.validity.valid
+    );
+  };
+
+  return (
+    <form id="login-step">
+      <h2>Login information</h2>
+      <ul>
+        <li>
+          <label htmlFor="username">* Username:</label>
+          <div>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              required
+              pattern="^[a-z]+$"
+              className={
+                username.untouched || username.valid ? "" : "invalid-input"
+              }
+              onChange={handleUsernameChange}
+              onBlur={() =>
+                setUsername({
+                  ...username,
+                  untouched: false,
+                })
+              }
+            />
+            <div
+              className="info"
+              style={
+                username.untouched || username.valid
+                  ? undefined
+                  : { color: "#ef4444" }
+              }
+            >
+              Username is required and must be in lowercase
+            </div>
+          </div>
+        </li>
+        <br />
+        <li>
+          <label htmlFor="password">* Password:</label>
+          <div>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password.value}
+              required
+              minLength={5}
+              pattern="(?=.*[a-zA-Z])(?=.*\d).*"
+              className={
+                password.untouched || password.valid ? "" : "invalid-input"
+              }
+              onChange={handlePwdChange}
+              onBlur={() => {
+                setPassword({
+                  ...password,
+                  untouched: false,
+                });
+              }}
+            />
+            <div
+              className="info"
+              style={
+                password.untouched || password.valid
+                  ? undefined
+                  : { color: "#ef4444" }
+              }
+            >
+              Password is required and must be at least 5 characters long and
+              contain at least one number and one letter
+            </div>
+          </div>
+        </li>
+        <br />
+        <li>
+          <label htmlFor="confirm-password">* Confirm password:</label>
+          <div>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              value={confirmPassword.value}
+              required
+              pattern={password.value}
+              className={
+                confirmPassword.untouched || confirmPassword.valid
+                  ? ""
+                  : "invalid-input"
+              }
+              onChange={handleConfirmPwdChange}
+              onBlur={() =>
+                setConfirmPassword({
+                  ...confirmPassword,
+                  untouched: false,
+                })
+              }
+            />
+            <div
+              className="info"
+              style={
+                confirmPassword.untouched || confirmPassword.valid
+                  ? undefined
+                  : { color: "#ef4444" }
+              }
+            >
+              This field is required and passwords should match
+            </div>
+          </div>
+        </li>
+      </ul>
+    </form>
+  );
+}
